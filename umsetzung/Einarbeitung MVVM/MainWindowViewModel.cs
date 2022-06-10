2834bea48bde5e12b6cb07b19356324a07add7b5
Lian Studer <ln.studer@protonmail.ch>
2022-06-10 13:52:11 -0700

    einarbeitung mvvm

---
+++ umsetzung/Einarbeitung MVVM/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einarbeitung_MVVM
+{
+    public class MainWindowViewModel
+    {
+        public Person Person { get; }
+
+        public MainWindowViewModel()
+        {
+            Person = new Person()
+            {
+                Vorname = "Lian",
+                Nachname = "Studer",
+                Alter = 16
+            };
+        }
+    }
+}
